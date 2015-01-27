d01353a448f31d010e36e39f7550453479070f8e
Harry Qiu <hqiu@ehealthinnovation.org>
2015-01-27 09:36:45 -0500

    remove unused files and replace the TI liscense with MIT liecense

--- Source/cgm.c
+++ Source/cgm.c
@@ -1,41 +1,33 @@
 /**************************************************************************************************
 Filename:       cgm.c
 
-Revised:        $Date: $
-Revision:       $Revision:  $
+Revised:        Date: 2015-01-24
+Revision:       Revision:  1
 
 Description:    This file contains the CGM sensor simulator application
 for use with the CC2540 Bluetooth Low Energy Protocol Stack.
 
-Copyright 2011-2013 Texas Instruments Incorporated. All rights reserved.
+The MIT License (MIT)
 
-IMPORTANT: Your use of this Software is limited to those specific rights
-granted under the terms of a software license agreement between the user
-who downloaded the software, his/her employer (which must be your employer)
-and Texas Instruments Incorporated (the "License").  You may not use this
-Software unless you agree to abide by the terms of the License. The License
-limits your use, and you acknowledge, that the Software may not be modified,
-copied or distributed unless embedded on a Texas Instruments microcontroller
-or used solely and exclusively in conjunction with a Texas Instruments radio
-frequency transceiver, which is integrated into your product.  Other than for
-the foregoing purpose, you may not use, reproduce, copy, prepare derivative
-works of, modify, distribute, perform, display or sell this Software and/or
-its documentation for any purpose.
+Copyright (c) 2014-2015 Center for Global ehealthinnovation
 
-YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
-PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
-INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
-NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
-TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
-NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
-LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
-OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
-OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-Should you have any questions regarding your right to use this Software,
-contact Texas Instruments Incorporated at www.TI.com.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
  **************************************************************************************************/
 
 /*********************************************************************
